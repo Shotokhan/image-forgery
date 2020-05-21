635f951ebbfc446bda67316f7c9eeb63691a8949
Marco Carlo Feliciano <52707252+Shotokhan@users.noreply.github.com>
2020-05-22 01:14:47 +0200

    Update weighted_corr.m

--- correlation/weighted_corr.m
+++ correlation/weighted_corr.m
@@ -2,11 +2,9 @@ function y = weighted_corr(block_prnu,block_res,weights)
 % y = corr2(block_prnu, block_res);
 block_prnu = double(block_prnu);
 block_res = double(block_res);
-mean_bp = mean2(block_prnu .* weights);
-mean_br = mean2(block_res .* weights);
-block_prnu = block_prnu - mean_bp;
-block_res = block_res - mean_br;
-y = sum(sum(block_prnu .* block_res .* weights)) / (sqrt(sum(sum((block_prnu .* weights) .^ 2))) * sqrt(sum(sum((block_res .* weights) .^ 2))));
+m = @(x, w) sum(sum(x.*w)) / sum(w(:));
+cov = @(x,y,w) sum(sum(w.*(x-m(x,w)).*(y-m(y,w)))) / sum(w(:));
+y = cov(block_prnu, block_res, weights) / sqrt(cov(block_prnu, block_prnu, weights) * cov(block_res, block_res, weights));
 if isnan(y)
     y = 0;
 end
