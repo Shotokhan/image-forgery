8f007569e3586adf0870cbf407a0c2056b539317
Marco Carlo Feliciano <52707252+Shotokhan@users.noreply.github.com>
2020-05-22 12:38:10 +0200

    Optimize weighted_corr

--- correlation/weighted_corr.m
+++ correlation/weighted_corr.m
@@ -1,13 +1,14 @@
-function y = weighted_corr(block_prnu,block_res,weights)
+function y = weighted_corr(block_prnu,block_res,weights,sum_w)
 % y = corr2(block_prnu, block_res);
 block_prnu = double(block_prnu);
 block_res = double(block_res);
-m = @(x, w) sum(sum(x.*w)) / sum(w(:));
-cov = @(x,y,w) sum(sum(w.*(x-m(x,w)).*(y-m(y,w)))) / sum(w(:));
+m = @(x, w) sum(x.*w) / sum_w;
+mean_x = m(block_prnu,weights); 
+mean_y = m(block_res,weights);
+cov = @(x,y,w) sum(w.*(x-mean_x).*(y-mean_y)) / sum_w;
 y = cov(block_prnu, block_res, weights) / sqrt(cov(block_prnu, block_prnu, weights) * cov(block_res, block_res, weights));
 if isnan(y)
     y = 0;
 end
 % corr2 calcola la correlazione normalizzata, ma senza pesi
 end
-
