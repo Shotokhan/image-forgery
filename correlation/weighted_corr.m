f9620a5b4de1126216bdba12f69fdcac2e2b9189
Marco Carlo Feliciano <52707252+Shotokhan@users.noreply.github.com>
2020-05-28 15:39:18 +0200

    Update weighted_corr.m

--- correlation/weighted_corr.m
+++ correlation/weighted_corr.m
@@ -1,39 +1,14 @@
-function y = weighted_corr(prnu, img_under_test, weights, denoiser)
-    % Returns the correlation map between the estimated PRNU and the
-    % residual of the input image
-    % prnu: the estimated PRNU
-    % img_under_test: RGB image to test, "as-it-is" when it is read
-    % window_size: size of the sliding correlation window
-    % weights: window of (window_size)x(window_size) dimension of weights
-    % to apply to the correlation, this is for guided correlation
-    % denoiser: specifies denoiser to apply to compute the residual, you
-    % must take care to use a PRNU estimated with the same denoiser to have
-    % meaningful results; "bm3d" to use BM3D denoiser, default or nothing
-    % is Mihcak denoiser.
-    % TODO: check for good input
-    [M, N] = size(prnu);
-    res = zeros(M, N);
-    if ~exist('denoiser', 'var')
-        res = getResidue(img_under_test);
-    elseif denoiser == "bm3d"
-        res = getResidue_BM3D(img_under_test);
-    else
-        res = getResidue(img_under_test);
-    end
-
-    sum_w = sum(weights(:));
-    weights = weights / sum_w;
-
-    mean_x = imfilter(prnu, weights, 'symmetric');
-    mean_y = imfilter(res, weights, 'symmetric');
-    
-    mean_xy = imfilter(prnu.*res, weights, 'symmetric');
-    corr_xy = mean_xy - mean_x.*mean_y;
-    
-    mean_xx = imfilter(prnu.^2, weights, 'symmetric');
-    var_x = mean_xx - mean_x.^2;
-    mean_yy = imfilter(res.^2, weights, 'symmetric');
-    var_y = mean_yy - mean_y.^2;
-    
-    y = corr_xy ./ sqrt(var_x.*var_y);
+function y = weighted_corr(block_prnu,block_res,weights,sum_w)
+% y = corr2(block_prnu, block_res);
+block_prnu = double(block_prnu);
+block_res = double(block_res);
+m = @(x, w) sum(x.*w) / sum_w;
+mean_x = m(block_prnu,weights); 
+mean_y = m(block_res,weights);
+cov = @(x,y,w) sum(w.*(x-mean_x).*(y-mean_y)) / sum_w;
+y = cov(block_prnu, block_res, weights) / sqrt(cov(block_prnu, block_prnu, weights) * cov(block_res, block_res, weights));
+if isnan(y)
+    y = 0;
+end
+% corr2 calcola la correlazione normalizzata, ma senza pesi
 end
