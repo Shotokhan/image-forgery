21f673d75bb53ad57164e5cb810708477577949a
Marco Carlo Feliciano <52707252+Shotokhan@users.noreply.github.com>
2020-05-30 18:13:11 +0200

    Update batch_test.m

--- batchProcessing/batch_test.m
+++ batchProcessing/batch_test.m
@@ -3,7 +3,7 @@ function corr_mat = batch_test(prnu, frg_mat, denoiser, corr_type, win_size)
     if corr_type == "guided"
         corr_fun = @(img) getGuidedCorrelation(prnu, img, win_size, denoiser);
     else
-        corr_fun = @(img) weighted_getCorrelation(prnu, img, ones(win_size, win_size), denoiser);
+        corr_fun = @(img) getCorrelation(prnu, img, ones(win_size, win_size), denoiser);
     end
     [M,N,~,S] = size(frg_mat);
     corr_mat = zeros(M,N,S);
